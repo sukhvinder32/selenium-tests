ebc335d43b9de5273605e602b8fd296e8f3551a9
Sriram <SeshagiriSriram@gmail.com>
2017-01-06 19:44:22 +0000

    Added Code

---
+++ src/selenium_tests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Test;
+
+public class selenium_tests {
+
+	@Test
+	public void test() {
+			System.out.println("Successful");
+	}
+
+}
